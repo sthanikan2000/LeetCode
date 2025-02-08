e3049f85254354ad15584aa7696b9b1c2596e440
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-02-08 17:54:32 +0530

    [1280] [Time Beats: 57.99%] - LeetPush

--- SQL50/1280-Students-and-Examinations.sql
+++ SQL50/1280-Students-and-Examinations.sql
@@ -1,19 +1,15 @@
 # Write your MySQL query statement below
--- select * from (
---     students left join (
---         select student_id,subject_name,count(subject_name) as attended_exams 
---         from examinations group by student_id,subject_name
---     ) Exam_Count using(student_id) 
--- ) as X left join subjects using (subject_name);
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.student_id) AS attended_exams
+-- select *
+FROM Students s 
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
 
--- (select * from students,subjects) 
 
--- select students.student_id,students.student_name,subjects.subject_name, count(examinations.student_id) 
--- from students,subjects,examinations 
--- where examinations.student_id = students.id and examinations.subject_name = 
-
-select SS.*,IF(ExamCount.attended_exams is null, 0, ExamCount.attended_exams) attended_exams
-    from (select * from students,subjects) SS left join 
-            (select student_id, subject_name ,count(subject_name) as attended_exams from examinations group by student_id,subject_name) ExamCount
-                on SS.student_id=ExamCount.student_id and SS.subject_name=ExamCount.subject_name
-                order by SS.student_id,SS.subject_name;
+-- select SS.*,IF(ExamCount.attended_exams is null, 0, ExamCount.attended_exams) attended_exams
+--     from (select * from students,subjects) SS left join 
+--             (select student_id, subject_name ,count(subject_name) as attended_exams from examinations group by student_id,subject_name) ExamCount
+--                 on SS.student_id=ExamCount.student_id and SS.subject_name=ExamCount.subject_name
+--                 order by SS.student_id,SS.subject_name;
