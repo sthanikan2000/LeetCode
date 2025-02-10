97a2d334270596b775a5e93673bc734dc3140e50
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-02-10 10:19:07 +0530

    [570] [Time Beats: 85.44%] - LeetPush

--- SQL50/570-Managers-with-at-Least-5-Direct-Reports.sql
+++ SQL50/570-Managers-with-at-Least-5-Direct-Reports.sql
@@ -2,4 +2,4 @@
 -- select E.name from
 -- (select managerId from Employee where managerId is not null group by managerID having count(managerId)>=5) as R 
 -- inner join Employee E on R.managerId = E.id;
-
+select M.name from employee E inner join Employee M on E.managerId=M.id group by M.id having count(*)>=5;
