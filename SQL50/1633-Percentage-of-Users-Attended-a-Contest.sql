85238d81c843285d847ca8dcc8d07a9796d860ad
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-02-15 08:41:06 +0530

    [1633] [Time Beats: 49.12%] - LeetPush

--- SQL50/1633-Percentage-of-Users-Attended-a-Contest.sql
+++ SQL50/1633-Percentage-of-Users-Attended-a-Contest.sql
@@ -1,8 +1,14 @@
 # Write your MySQL query statement below
-select contest_id, ROUND(AVG(IF(DU.user_id=R.user_id,1,0))*100,2) percentage
-from (select distinct(contest_id) contest_id from Register) DC 
-cross join (select distinct(user_id) user_id from Users) DU
-left join Register R using(contest_id,user_id)
+-- select contest_id, ROUND(AVG(IF(DU.user_id=R.user_id,1,0))*100,2) percentage
+-- from (select distinct(contest_id) contest_id from Register) DC 
+-- cross join (select distinct(user_id) user_id from Users) DU
+-- left join Register R using(contest_id,user_id)
+-- group by contest_id
+-- order by percentage desc, contest_id asc
+-- ;
+
+select contest_id, Round((count(*)/(select count(*) from users))*100,2) percentage
+from Register
 group by contest_id
 order by percentage desc, contest_id asc
 ;
