f33a04b8cc94afec43ef6e8877417d1a36679e96
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-02-16 12:18:01 +0530

    [610] [Time Beats: 57.71%] - LeetPush

--- SQL50/610-Triangle-Judgement.sql
+++ SQL50/610-Triangle-Judgement.sql
@@ -1,3 +1,17 @@
 # Write your MySQL query statement below
-select x,y,z, IF(x+y>z and y+z>x and z+x>y, \Yes\, \No\) triangle
+-- select x,y,z, IF(x+y>z and y+z>x and z+x>y, \Yes\, \No\) triangle
+-- from Triangle;
+
+#CASE WHEN
+-- CASE
+--     WHEN condition1 THEN result1
+--     WHEN condition2 THEN result2
+--     WHEN conditionN THEN resultN
+--     ELSE result
+-- END;
+select *,
+CASE 
+    WHEN x+y>z and x+z>y and y+z>x THEN 'Yes'
+    ELSE 'No'
+END triangle
 from Triangle;
