8b08c2a224feebc55278316359db5a67bf2a890e
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-02-17 09:00:20 +0530

    [1164] [Time Beats: 91.12%] - LeetPush

--- SQL50/1164-Product-Price-at-a-Given-Date.sql
+++ SQL50/1164-Product-Price-at-a-Given-Date.sql
@@ -1,13 +1,25 @@
 # Write your MySQL query statement below
-select distinct(product_id) product_id, IFNULL(CP.new_price,10) price
-from Products P
-left  join 
-    (select P.product_id, P.new_price
-    from Products P
-    inner join 
-        (select product_id,max(change_date) change_date
-        from Products
-        where change_date<="2019-08-16"
-        group by product_id) CD
-    on P.product_id=CD.product_id and P.change_date=CD.change_date) CP
-using (product_id);
+-- select distinct(product_id) product_id, IFNULL(CP.new_price,10) price
+-- from Products P
+-- left  join 
+--     (select P.product_id, P.new_price
+--     from Products P
+--     inner join 
+--         (select product_id,max(change_date) change_date
+--         from Products
+--         where change_date<="2019-08-16"
+--         group by product_id) CD
+--     on P.product_id=CD.product_id and P.change_date=CD.change_date) CP
+-- using (product_id);
+
+
+### Solution from discussion
+select distinct product_id, 10 price from Products 
+where product_id not in(select distinct product_id from Products where change_date <='2019-08-16' )
+
+union 
+
+select product_id, new_price price from Products 
+where (product_id,change_date) in 
+    (select product_id , max(change_date) date from Products where change_date <='2019-08-16' group by product_id)
+;
