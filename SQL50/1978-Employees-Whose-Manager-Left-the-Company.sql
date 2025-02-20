ba46ad78803f4750549dde11c096075fdd45bde6
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-02-20 10:47:06 +0530

    [1978] [Time Beats: 92.78%] - LeetPush

--- SQL50/1978-Employees-Whose-Manager-Left-the-Company.sql
+++ SQL50/1978-Employees-Whose-Manager-Left-the-Company.sql
@@ -1,2 +1,5 @@
-# Write your MySQL query statement below
-select employee_id from Employees where salary<30000 and manager_id not in (select distinct(employee_id) from Employees) order by employee_id; 
+-- # Write your MySQL query statement below
+-- select employee_id from Employees where salary<30000 and manager_id not in (select distinct(employee_id) from Employees) order by employee_id; 
+
+# No need of distinct, because employee_id is the primary key
+select employee_id from Employees where salary<30000 and manager_id not in (select employee_id from Employees) order by employee_id; 
