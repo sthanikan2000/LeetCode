5527afb8e9a3f678906b16236467c304df6c9985
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-13 21:41:04 +0530

    [125] [Time Beats: 5.22%] [Memory Beats: 70.14%] - LeetPush

--- Interview150/Valid-Palindrome.py
+++ Interview150/Valid-Palindrome.py
@@ -1,3 +1,4 @@
+ALPHA_NUMERIC_RANGE=list(range(48,58))+list(range(65,91))+list(range(97,123))
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         if not s:
@@ -7,10 +8,10 @@ class Solution:
         while sta<end:
             ord_sta=ord(s[sta])
             ord_end=ord(s[end])
-            if ord_sta not in range(97,123) and ord_sta not in range(65,91) and ord_sta not in range(48,58):
+            if ord_sta not in ALPHA_NUMERIC_RANGE:
                 sta+=1
                 continue
-            if ord_end not in range(97,123) and ord_end not in range(65,91) and ord_end not in range(48,58):
+            if ord_end not in ALPHA_NUMERIC_RANGE:
                 end-=1
                 continue
             if s[sta].lower() != s[end].lower():
