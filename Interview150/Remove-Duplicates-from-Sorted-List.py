f5b68c4881629bc8fe973289a9f792618437f458
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-13 22:45:55 +0530

    [83] [Time Beats: 100.00%] [Memory Beats: 21.96%] - LeetPush

---
+++ Interview150/Remove-Duplicates-from-Sorted-List.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        head_=head
+        while head_.next and head_.next.val == head_.val:
+            head_=head_.next
+        # Now we have our head
+
+        if not head_.next:
+            return head_
+
+        p1 = head_
+        p2 = head_.next 
+
+        while p2 and p2.next:
+            if p2.val == p2.next.val:
+                p2 = p2.next
+            else:
+                p1.next=p2
+                p1=p2
+                p2=p2.next
+        p1.next = p2
+        return head_
+            
+
+        
+
+        
