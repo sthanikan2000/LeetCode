7b98e44785b2398eab14078269edc41e757aa789
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-14 02:02:47 +0530

    [190] [Time Beats: 32.96%] [Memory Beats: 35.95%] - LeetPush

--- Interview150/Reverse-Bits.py
+++ Interview150/Reverse-Bits.py
@@ -1,8 +1,18 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        bin_ = bin(n)[2:]
-        bin_ = [\0\]*(32-len(bin_))+list(bin_)
+        # bin_ = bin(n)[2:]
+        # bin_ = [\0\]*(32-len(bin_))+list(bin_)
         
-        for i in range(0,16):
-            bin_[i],bin_[31-i] = bin_[31-i],bin_[i]
-        return int(\\.join(bin_),2)      
+        # for i in range(0,16):
+        #     bin_[i],bin_[31-i] = bin_[31-i],bin_[i]
+        # return int(\\.join(bin_),2)  
+
+        ######  ---------- Alternate solution using bit operations -----------------
+        result = 0
+        for _ in range(32):
+            lsb = n & 1
+            # result = (result<<1)+lsb
+            result = (result<<1) | lsb 
+            n = n >> 1
+        return result
+
