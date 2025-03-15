054f9d55ab9e6e358d69199c244a39e991c872bd
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-15 12:57:36 +0530

    [13] [Time Beats: 47.13%] [Memory Beats: 90.61%] - LeetPush

--- Interview150/Roman-to-Integer.py
+++ Interview150/Roman-to-Integer.py
@@ -1,31 +1,43 @@
+# USAGE OF STACK
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+#         mapping = {\I\:1, \V\:5, \X\:10, \L\:50, \C\:100, \D\:500, \M\:1000 }
+#         result = 0
+#         stack = []
+#         for i in range(0,len(s)-1):
+#             if not stack and mapping[s[i]]>=mapping[s[i+1]]:
+#                 result += mapping[s[i]]
+#             elif stack and mapping[s[i]]>=mapping[s[i+1]]:
+#                 temp = mapping[s[i]]
+#                 while stack:
+#                     temp -= mapping[stack.pop(-1)]
+#                 result += temp
+#             # elif not stack and mapping[s[i]]<mapping[s[i+1]]:
+#             #     stack.append(s[i])
+#             else:
+#                 stack.append(s[i])
+#         # print(result,stack,i)
+#         if stack:
+#             temp = mapping[s[len(s)-1]]
+#             while stack:
+#                 temp -= mapping[stack.pop(-1)]
+#             result += temp
+#         else:
+#             result += mapping[s[len(s)-1]]
+#         return result
+
 class Solution:
     def romanToInt(self, s: str) -> int:
-        mapping = {\I\:1, \V\:5, \X\:10, \L\:50, \C\:100, \D\:500, \M\:1000 }
-        result = 0
-        stack = []
-        for i in range(0,len(s)-1):
-            if not stack and mapping[s[i]]>=mapping[s[i+1]]:
-                result += mapping[s[i]]
-            elif stack and mapping[s[i]]>=mapping[s[i+1]]:
-                temp = mapping[s[i]]
-                while stack:
-                    temp -= mapping[stack.pop(-1)]
-                result += temp
-            # elif not stack and mapping[s[i]]<mapping[s[i+1]]:
-            #     stack.append(s[i])
+        values = {\I\:1, \V\:5, \X\:10, \L\:50, \C\:100, \D\:500, \M\:1000}
+        total = values[s[0]]
+        pre = s[0]
+        for i in s[1:]:
+            if values[pre] >= values[i]:
+                total += values[i]
+                pre = i
             else:
-                stack.append(s[i])
-        # print(result,stack,i)
-        if stack:
-            temp = mapping[s[len(s)-1]]
-            while stack:
-                temp -= mapping[stack.pop(-1)]
-            result += temp
-        else:
-            result += mapping[s[len(s)-1]]
-        return result
-
-
-            
+                total += values[i] - 2*values[pre]
+                pre = i
+        return total
            
         
