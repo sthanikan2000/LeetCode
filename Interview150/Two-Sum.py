c1697f25377bfa60da16d8cb7c7052213321ee86
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-16 22:39:18 +0530

    [1] [Time Beats: 100.00%] [Memory Beats: 6.40%] - LeetPush

--- Interview150/Two-Sum.py
+++ Interview150/Two-Sum.py
@@ -1,7 +1,6 @@
-# With the Idea of Hash Table
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict_={}
+        dict_={} # With the Idea of Hash Table
         dict_[(target-nums[0])]=0
         for i in range(1,len(nums)):
             if nums[i] in dict_:
