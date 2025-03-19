c862cce6d057793d9469b0f7ca1dab5509260973
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-19 09:14:10 +0530

    [169] [Time Beats: 38.58%] [Memory Beats: 87.44%] - LeetPush

--- Daily_Problem/Majority-Element.py
+++ Daily_Problem/Majority-Element.py
@@ -4,7 +4,7 @@ class Solution:
             return nums[0]
         count = {} #Space Complexity: O(n)
         con=len(nums)//2
-        for i in nums:
+        for i in nums: #Time Complexity: O(n)
             if i in count:
                 count[i] +=1
                 if count[i]>con:
@@ -12,7 +12,7 @@ class Solution:
             else:
                 count[i]=1
 
-            
+        ### How to solve the problem in linear time and in O(1) space ?
         
         
         
