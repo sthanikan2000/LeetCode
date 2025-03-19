11ba346bc1ee27840a31ea0b16b679a1321116ec
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-19 08:40:48 +0530

    [278] [Time Beats: 58.51%] [Memory Beats: 26.97%] - LeetPush

--- Daily_Problem/First-Bad-Version.py
+++ Daily_Problem/First-Bad-Version.py
@@ -5,15 +5,18 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         l=1
         r=n
-        bad=0
-        while l<=r:
+        # bad=0
+        # while l<=r:
+        while l<r:
             mid = (l+r)//2
             if isBadVersion(mid):
-                bad=mid
-                r=mid-1
+                # bad=mid
+                # r=mid-1
+                r=mid
             else:
                 l=mid+1
-        return bad
+        # return bad
+        return r
             
 
 
