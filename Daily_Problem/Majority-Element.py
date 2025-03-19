ff3056cea7eaa40178c365aafb2e2e5b1ffeb0d1
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-19 09:31:23 +0530

    [169] [Time Beats: 56.35%] [Memory Beats: 19.99%] - LeetPush

--- Daily_Problem/Majority-Element.py
+++ Daily_Problem/Majority-Element.py
@@ -1,5 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        '''
         if len(nums)==1:
             return nums[0]
         count = {} #Space Complexity: O(n)
@@ -11,8 +12,25 @@ class Solution:
                     return i
             else:
                 count[i]=1
-
+        '''
         ### How to solve the problem in linear time and in O(1) space ?
+
+        # Since it is gurantee that majority element appears more than floor(n/2)
+        # we can pair the majority element with other elements
+        # and then their will atleast one majority elements needs a pair
+        majority = 0
+        majority_ele = None
+        for num in nums:
+            if majority == 0:
+                majority +=1
+                majority_ele=num
+                continue
+            if num != majority_ele:
+                majority -= 1
+            else:
+                majority += 1
+        return majority_ele
+            
         
         
         
