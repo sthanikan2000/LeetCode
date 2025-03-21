363d17c392e9d652e4057ad104012cdedc9dc518
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-21 23:17:37 +0530

    [104] [Time Beats: 100.00%] [Memory Beats: 79.19%] - LeetPush

---
+++ Interview150/Maximum-Depth-of-Binary-Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        ### Breath First Search
+        if (not root):
+            return 0
+        q=[root]
+        d=0
+        while q:
+            # len_q = len(q)
+            for i in range(len(q)):
+                x = q.pop(0)
+                if x.left:
+                    q.append(x.left)
+                if x.right:
+                    q.append(x.right)
+            d+=1
+        return d
+
+        
