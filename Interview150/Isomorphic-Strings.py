815e947914492afed79c2ffb94d668b48a7305c8
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-22 20:43:02 +0530

    [205] [Time Beats: 95.98%] [Memory Beats: 55.03%] - LeetPush

--- Interview150/Isomorphic-Strings.py
+++ Interview150/Isomorphic-Strings.py
@@ -2,6 +2,19 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
+        ### Method 1
+        s_to_t = {}
+        assigned = []
+        for i in range(len(s)):
+            x = s_to_t.get(s[i])
+            if x == None and t[i] not in assigned:
+                s_to_t[s[i]] = t[i]
+                assigned.append(t[i])
+            elif x != t[i]:
+                return False
+        return True
+        '''
+        ### Method 2
         f_occ_1 = {}
         f_occ_2 = {}
         for i in range(len(s)):
@@ -13,3 +26,4 @@ class Solution:
             elif x != y:
                 return False     
         return True
+        '''
