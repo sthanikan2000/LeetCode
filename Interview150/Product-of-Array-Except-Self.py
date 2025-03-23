3116951663f24c955cd37858ad0bd72a267ab49f
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-23 14:59:45 +0530

    [238] [Time Beats: 90.06%] [Memory Beats: 79.45%] - LeetPush

--- Interview150/Product-of-Array-Except-Self.py
+++ Interview150/Product-of-Array-Except-Self.py
@@ -1,6 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        res = [0]*n
+        res[1] = nums[0]
+        for i in range(2,n):
+            res[i] = res[i-1] * nums[i-1]
+        x = nums[n-1]
+        # print(res)
+        for i in range(n-2,0,-1):
+            # print(i,x)
+            res[i] *= x
+            x *= nums[i]
+        res[0] = x
+        return res
+        '''
+        ### Not memory Efficient
+        n = len(nums)
         pro_1 = [0]*(n-1)
         pro_2 = [0]*(n-1)
         pro_1[0] = nums[0]
@@ -16,5 +31,6 @@ class Solution:
         for i in range(n-2):
             res[i+1] = pro_1[i]*pro_2[i+1]
         return res
+        '''
 
         
