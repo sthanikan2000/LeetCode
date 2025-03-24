efbea2a9370b99b8f0f816ff59c00d3783ceea09
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-24 09:03:30 +0530

    [274] [Time Beats: 100.00%] [Memory Beats: 64.04%] - LeetPush

--- Interview150/H-Index.py
+++ Interview150/H-Index.py
@@ -1,7 +1,7 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         '''
-        ### Method 1
+        ### Method 1 - Reverse sort & linear search
         citations.sort(reverse=True)
         len_c =len(citations)
         i = 0 
@@ -11,6 +11,8 @@ class Solution:
             i+=1
         return i
         '''
+        '''
+        ### Method 2 - Sorting & Linear Search
         citations.sort()
         if citations[-1] == 0:
             return 0
@@ -18,3 +20,17 @@ class Solution:
         for i,c in enumerate(citations):
             if c >= len_c-i:
                 return len_c-i
+        '''
+        ### Method 3 - Sorting & Binary search
+        citations.sort(reverse = True)
+        l = 0
+        r = len(citations) - 1
+        h_i = 0
+        while l<=r:
+            mid = (l+r)//2
+            if citations[mid]>= mid+1:
+                h_i = mid+1
+                l= mid+1
+            else:
+                r = mid-1
+        return h_i
