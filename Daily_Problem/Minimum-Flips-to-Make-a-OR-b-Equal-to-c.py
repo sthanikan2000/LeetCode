1a8b1f705b1f8d3243fab45c1f7229e834cabab8
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-27 21:43:39 +0530

    [1318] [Time Beats: 89.93%] [Memory Beats: 10.80%] - LeetPush

--- Daily_Problem/Minimum-Flips-to-Make-a-OR-b-Equal-to-c.py
+++ Daily_Problem/Minimum-Flips-to-Make-a-OR-b-Equal-to-c.py
@@ -5,9 +5,10 @@ class Solution:
         a_or_b_xor_c = a_or_b ^ c
         
         c = 0
-        while a_or_b_xor_c>0:
+        while a_or_b_xor_c:
             if a_or_b_xor_c & 1:
-                if a_or_b & 1 == 1 and a_and_b & 1 ==1:
+                if a_and_b & 1:
+                    # if a_or_b & 1 == 1 and a_and_b & 1 ==1:
                     c+=2
                 else:
                     c+=1
