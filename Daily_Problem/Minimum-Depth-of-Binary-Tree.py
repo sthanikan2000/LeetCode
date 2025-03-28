b9d221ebf2ccd6821e960d484cb704300991ced4
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-28 20:00:58 +0530

    [111] [Time Beats: 78.74%] [Memory Beats: 66.68%] - LeetPush

--- Daily_Problem/Minimum-Depth-of-Binary-Tree.py
+++ Daily_Problem/Minimum-Depth-of-Binary-Tree.py
@@ -8,6 +8,25 @@ class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
+        d = 0 
+        q = [root]
+        while q:
+            temp_lst = []
+            for node in q:
+                if not node.left and not node.right:
+                    return d+1
+                if node.left:
+                    temp_lst.append(node.left)
+                if node.right:
+                    temp_lst.append(node.right)
+            q = temp_lst
+            d+=1
+                
+
+        '''
+        ### Recursion Approach but not Optimal
+        if not root:
+            return 0
         # if not root.left and not root.right:
         #     return 1
         if not root.left:
@@ -15,4 +34,5 @@ class Solution:
         if not root.right:
             return 1 + self.minDepth(root.left)
         return 1 + min(self.minDepth(root.right),self.minDepth(root.left))
+        '''
         
