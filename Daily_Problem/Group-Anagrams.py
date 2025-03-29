eb092c1f2d05c36b63b5e006baca441244c9ea6e
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-29 12:20:03 +0530

    [49] [Time Beats: 98.04%] [Memory Beats: 43.98%] - LeetPush

--- Daily_Problem/Group-Anagrams.py
+++ Daily_Problem/Group-Anagrams.py
@@ -1,28 +1,37 @@
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         '''
+        ######################## DEFAULT DICTIONARY ########################################
+        >>> s = defaultdict()
+        >>> s
+            defaultdict(None, {})
+        ------------------------------------------------------------------------------------
+        -> A sub-class of the dictionary class that returns a dictionary-like object.
+        ------------------------------------------------------------------------------------
+        >>> s = defaultdict(int)
+        >>> s
+            defaultdict(<class 'int'>, {})
+        >>> s[2]
+            0
+        '''
+
+        '''
         ### Time Complexity: O(n*m)
-        c_map = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
-        str_map = {}
+        str_map = defaultdict(list)
         for s in strs:
             map_key = [0]*26
             for c in s:
-                map_key[c_map[c]] +=1
-            map_key = tuple(map_key)
-            lst = str_map.get(map_key,[])
-            lst.append(s)
-            str_map[map_key] = lst
+                map_key[ord(c)-ord(\a\)] +=1
+            str_map[tuple(map_key)].append(s)
         return list(str_map.values())
         '''
         
         ### Time Complexity O(n*m*log(m))
-        mapping = {}
+        mapping = defaultdict(list)
         for s in strs: ### n iterations
             map_key = tuple(sorted(s)) ### O(m*log(m)) ; 0<=m<=100
-            lst = mapping.get(map_key,[])
-            lst.append(s)
-            mapping[map_key]=lst
+            mapping[map_key].append(s)
         return list(mapping.values())
-        
 
         
