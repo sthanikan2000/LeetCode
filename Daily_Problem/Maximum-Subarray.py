649fcbba3b5687a17178050297ebfe9c76acfcca
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-29 10:57:13 +0530

    [53] [Time Beats: 95.64%] [Memory Beats: 23.77%] - LeetPush

--- Daily_Problem/Maximum-Subarray.py
+++ Daily_Problem/Maximum-Subarray.py
@@ -2,11 +2,11 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         max_sum = nums[0]
         current_sum = nums[0]
-        for num in nums[1:]:
+        for i in range(1,len(nums)):
             if current_sum<0:
-                current_sum = num
+                current_sum = nums[i]
             else:
-                current_sum += num
+                current_sum += nums[i]
             if max_sum<current_sum:
                 max_sum = current_sum
         return max_sum
