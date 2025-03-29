6771c83cedf3ce8e23cb9a25812a030d792ce3de
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-29 11:46:21 +0530

    [49] [Time Beats: 83.40%] [Memory Beats: 43.98%] - LeetPush

--- Daily_Problem/Group-Anagrams.py
+++ Daily_Problem/Group-Anagrams.py
@@ -1,8 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ### Time Complexity O(n*m*log(m))
         mapping = {}
-        for s in strs:
-            map_key = \\.join(sorted(list(s)))
+        for s in strs: ### n iterations
+            map_key = tuple(sorted(list(s))) ### O(m*log(m)) ; 0<=m<=100
             lst = mapping.get(map_key,[])
             lst.append(s)
             mapping[map_key]=lst
