8adf1129ec205ca9309d662884ed3d8d37104a6e
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-03-29 18:26:53 +0530

    [56] [Time Beats: 50.40%] [Memory Beats: 63.00%] - LeetPush

--- Daily_Problem/Merge-Intervals.py
+++ Daily_Problem/Merge-Intervals.py
@@ -2,13 +2,10 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key = lambda x: x[0])
         res = [intervals[0]]
-        # s,e = intervals[0]
-        # for interval in intervals[1:]:
         for i in range(1,len(intervals)):
             if intervals[i][0]>res[-1][1]:
                 res.append(intervals[i])
             elif intervals[i][1]>res[-1][1]:
                 res[-1][1] = intervals[i][1]
-        # res.append([s,e])
         return res
         
