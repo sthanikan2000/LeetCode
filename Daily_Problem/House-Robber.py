120d99a3a515ded3b73c79921604f9045852603c
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-04-01 17:55:24 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 54.32%] - LeetPush

--- Daily_Problem/House-Robber.py
+++ Daily_Problem/House-Robber.py
@@ -1,15 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        ### Best Solution with time complexity: O(n)
         n =len(nums)
         if n<=1:
             return nums[0]
-
-        dp = [0]*n
-        dp[n-1] = nums[n-1]
-        dp[n-2] = max(nums[n-2],nums[n-1])
+        max_rob_from = [0]*n
+        max_rob_from[n-1] = nums[n-1]
+        max_rob_from[n-2] = max(nums[n-2],nums[n-1])
         for i in range(n-3,-1,-1):
-            dp[i] = max(dp[i+1],nums[i]+dp[i+2])
-        return dp[0]
+            max_rob_from[i] = max(max_rob_from[i+1],nums[i]+max_rob_from[i+2])
+        return max_rob_from[0]
 
         '''
         ### Simple brute force solution
