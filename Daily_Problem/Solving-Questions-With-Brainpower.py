3bd3f072a6ab9e473288e262435c78318f27d22d
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-04-01 17:26:07 +0530

    [2140] [Time Beats: 99.46%] [Memory Beats: 98.81%] - LeetPush

--- Daily_Problem/Solving-Questions-With-Brainpower.py
+++ Daily_Problem/Solving-Questions-With-Brainpower.py
@@ -1,5 +1,19 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
+        ### Computation from right to left of array with time complexity O(n)
+        n = len(questions)
+        most_points_can_get_from_current = [0]*n
+        most_points_can_get_from_current[n-1] = questions[n-1][0]
+        for i in range(n-2,-1,-1):
+            max_skip_current = most_points_can_get_from_current[i+1]
+            max_include_current = questions[i][0]
+            if i+1+questions[i][1]<n:
+                max_include_current += most_points_can_get_from_current[i+1+questions[i][1]]
+            most_points_can_get_from_current[i] = max(max_skip_current,max_include_current)
+        return most_points_can_get_from_current[0]
+    
+    '''
+    ### Usage of recursion and reusage of already computed results
         i = 0
         already_computed = {}
         return self.findMaxPoints(questions,i,already_computed)
@@ -23,3 +37,4 @@ class Solution:
         already_computed[i] = max_points
         # print(already_computed)
         return max_points
+    '''
