a84644a92aa9750ca9fa92e3d859273a0a7c84cb
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-04-02 12:09:54 +0530

    [2873] [Time Beats: 100.00%] [Memory Beats: 54.86%] - LeetPush

---
+++ Daily_Problem/Maximum-Value-of-an-Ordered-Triplet-I.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        n = len(nums)
+        max_ele = nums[0]
+        max_diff = nums[0]-nums[1]
+        max_val = max_diff*nums[2]
+        for i in range(3,n):
+            max_ele = max(max_ele,nums[i-2])
+            max_diff = max(max_diff,max_ele-nums[i-1])
+            max_val = max(max_val,max_diff*nums[i])
+        if max_val<0:
+            return 0
+        return max_val
+
+
+
+
+        
