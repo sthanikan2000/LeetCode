19938ad6fa69d6e365e5c9e740e93baa55fc2b0d
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-04-03 13:08:54 +0530

    [1356] [Time Beats: 21.10%] [Memory Beats: 28.40%] - LeetPush

--- Daily_Problem/Sort-Integers-by-The-Number-of-1-Bits.py
+++ Daily_Problem/Sort-Integers-by-The-Number-of-1-Bits.py
@@ -1,11 +1,12 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
-        def one_bits(x:int) -> int:
+        def custom_sort_key(x):
+            x0 = x
             c = 0 
             while x>0:
                 c += (x&1)
                 x >>= 1
-            return c
-        arr.sort(key=lambda x:(one_bits(x),x))
+            return (c,x0)
+        arr.sort(key=custom_sort_key)
         return arr
         
