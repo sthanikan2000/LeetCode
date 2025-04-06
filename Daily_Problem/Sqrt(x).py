fc7aaaed58b585bf34d630c04d16f956b52c6bc4
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-04-06 21:29:29 +0530

    [69] [Time Beats: 100.00%] [Memory Beats: 52.31%] - LeetPush

--- Daily_Problem/Sqrt(x).py
+++ Daily_Problem/Sqrt(x).py
@@ -1,9 +1,7 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0:
-            return 0
-        if x == 1:
-            return 1
+        if x == 0 or x == 1:
+            return x
         s = 1
         e = x//2
         while s<=e:
