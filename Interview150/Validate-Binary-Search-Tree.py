db40c907c403e5554c33a1f9cb97ff7fac1de365
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-04-08 20:10:46 +0530

    [98] [Time Beats: 100.00%] [Memory Beats: 90.11%] - LeetPush

--- Interview150/Validate-Binary-Search-Tree.py
+++ Interview150/Validate-Binary-Search-Tree.py
@@ -6,22 +6,23 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
         ### If InOrderTraversal return ascending order, then tree is valid BST
-        def ascendingInOrder(root: Optional[TreeNode],lst = [])->bool:
+        prev = [float(\-inf\)]
+        def ascendingInOrder(root: Optional[TreeNode], lst)->bool:
             if not root:
                 return True
             if root.left:
                 if not ascendingInOrder(root.left,lst):
                     return False
-            if lst and lst[-1]>=root.val:
-                print(lst)
+            if lst[0]>=root.val:
                 return False
-            lst.append(root.val)
+            lst[0] = root.val
             if root.right:
                 if not ascendingInOrder(root.right,lst):
                     return False
             return True
-        return ascendingInOrder(root)
+        return ascendingInOrder(root,prev)
         
 
         
