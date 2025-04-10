dbd56806df7f89705174c7d1a789c788846fb473
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-04-10 11:55:56 +0530

    [530] [Time Beats: 56.02%] [Memory Beats: 25.08%] - LeetPush

--- Interview150/Minimum-Absolute-Difference-in-BST.py
+++ Interview150/Minimum-Absolute-Difference-in-BST.py
@@ -6,6 +6,29 @@
 #         self.right = right
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        ### InOrderTraversal - Iteration Appraoch
+        stack = [root] # Stack
+        min_diff=float(\inf\)
+        prev = None
+        while stack:
+            current = stack[-1]
+            while current.left:
+                stack.append(current.left)
+                current.left = None
+                current=stack[-1]
+            if prev != None:
+                diff = abs(prev-current.val)
+                if diff<min_diff:
+                    min_diff = diff
+            prev = current.val
+            stack.pop()            
+            if current.right:
+                stack.append(current.right)
+        return min_diff
+            
+
+        '''
+        ### InOrderTraversal - Recursion Approach
         g_sto = {\prev\:None,\min_diff\:float(\inf\)}
         def InOrder(root,storage):
             if root:
@@ -20,6 +43,7 @@ class Solution:
                 InOrder(root.right,storage)
         InOrder(root,g_sto)
         return g_sto[\min_diff\]
+        '''
 
 
         
