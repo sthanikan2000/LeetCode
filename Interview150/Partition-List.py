712f949ffdf9a035893960bb361a8ee73d6c59ff
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-04-13 20:06:31 +0530

    [86] [Time Beats: 16.72%] [Memory Beats: 41.32%] - LeetPush

---
+++ Interview150/Partition-List.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        l_head = None
+        l_tail = None
+        r_head = None
+        r_tail = None
+        current=head
+        while current:
+            if current.val < x:
+                if l_head:
+                    l_tail.next = current
+                    current = current.next
+                    l_tail = l_tail.next
+                    l_tail.next = None
+                else:
+                    temp = current
+                    current = current.next
+                    temp.next = None
+                    l_head = l_tail = temp
+            else:
+                if r_head:
+                    r_tail.next = current
+                    current = current.next
+                    r_tail = r_tail.next
+                    r_tail.next = None
+                else:
+                    temp = current
+                    current = current.next
+                    temp.next = None
+                    r_head = r_tail = temp
+
+        if l_head and r_head:
+            l_tail.next = r_head
+        elif r_head:
+            return r_head
+        return l_head
+
+
+
+
+
+        
