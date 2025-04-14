16519fea08d934c9d85554433b989d49a3ac925e
Thanikan Sivatheepan <thanikan.20@cse.mrt.ac.lk>
2025-04-14 21:20:50 +0530

    [138] [Time Beats: 19.77%] [Memory Beats: 41.66%] - LeetPush

---
+++ Interview150/Copy-List-with-Random-Pointer.py
@@ -0,0 +1,26 @@
+\\\
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+\\\
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return 
+        old_new_ref = {}
+        current = head
+        while current:
+            node = Node(current.val)
+            old_new_ref[current]=node
+            current = current.next
+        for old_node in old_new_ref.keys():
+            if old_node.next:
+                old_new_ref[old_node].next = old_new_ref[old_node.next]
+            if old_node.random:
+                old_new_ref[old_node].random = old_new_ref[old_node.random]
+        return old_new_ref[head]
+        
